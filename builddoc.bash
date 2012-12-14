d05112b168f525b3991672272928544e8543237f
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-12-14 16:52:37 -0500

    getting closer to Chicago style

--- builddoc.bash
+++ builddoc.bash
@@ -5,6 +5,4 @@ biber data-quality-workshop-giarlo
 latex data-quality-workshop-giarlo
 latex data-quality-workshop-giarlo
 pdflatex data-quality-workshop-giarlo
-acroread data-quality-workshop-giarlo.pdf &
-#pandoc -o data-quality-workshop-giarlo.odt data-quality-workshop-giarlo.tex
-#loffice data-quality-workshop-giarlo.odt &
+evince data-quality-workshop-giarlo.pdf &
