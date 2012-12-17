6a9c05c12addc3cb951156a0dc816ddc9f56cced
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-12-17 10:45:27 -0500

    adds pandoc to build script; adds pandoc-generated and lightly hand-modified ODT artifact to version control (submitted to JESL); corrects in-text Curry cite from autocite to textcite

--- builddoc.bash
+++ builddoc.bash
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Compile the document & bibliography
 latex data-quality-workshop-giarlo
 biber data-quality-workshop-giarlo
 latex data-quality-workshop-giarlo
 latex data-quality-workshop-giarlo
+
+# Generate a PDF artifact
 pdflatex data-quality-workshop-giarlo
+
+# Generate an office document artifact
+pandoc -o data-quality-workshop-giarlo.odt --bibliography=data-quality-workshop-giarlo.bib data-quality-workshop-giarlo.tex
+
+# Open the PDF to make sure it looks right
 evince data-quality-workshop-giarlo.pdf &
