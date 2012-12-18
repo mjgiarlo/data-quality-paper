b17224645926ff32b478bab1bdd8a910edf058ed
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-12-17 21:44:09 -0500

    removes odt artifact, updates PDF artifact, temporarily disables pandoc in build script

--- builddoc.bash
+++ builddoc.bash
@@ -10,7 +10,7 @@ latex data-quality-workshop-giarlo
 pdflatex data-quality-workshop-giarlo
 
 # Generate an office document artifact
-pandoc -o data-quality-workshop-giarlo.odt --bibliography=data-quality-workshop-giarlo.bib data-quality-workshop-giarlo.tex
+#pandoc -o data-quality-workshop-giarlo.odt --bibliography=data-quality-workshop-giarlo.bib data-quality-workshop-giarlo.tex
 
 # Open the PDF to make sure it looks right
 evince data-quality-workshop-giarlo.pdf &
